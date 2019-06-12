f207418a1c9421149b1ab6b1e029dbf4f20f4a0a
ikesh pack <packappsforyou@gmail.com>
2019-06-12 19:10:27 -0400

    Added user model associations

--- polls_app/app/models/user.rb
+++ polls_app/app/models/user.rb
@@ -10,6 +10,14 @@
 
 class User < ApplicationRecord
     
-    
- 
+    has_many :authored_polls,
+        class_name: :Polls,
+        primary_key: :id,
+        foreign_key: :author_id
+
+    has_many :responses,
+        class_name: :Response,
+        primary_key: :id,
+        foreign_key: :user_id    
+
 end
