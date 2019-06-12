ae853e0419a21d1ddce993ab6d26890c117d255d
ikesh pack <packappsforyou@gmail.com>
2019-06-12 19:22:43 -0400

    Added polls model validation and associations

--- polls_app/app/models/answer_choices.rb
+++ polls_app/app/models/answer_choices.rb
@@ -13,4 +13,5 @@ class AnswerChoices < ApplicationRecord
 
 
 
+
 end
--- polls_app/app/models/question.rb
+++ polls_app/app/models/question.rb
@@ -10,5 +10,18 @@
 #
 
 class Question < ApplicationRecord
+    
+    validates :body, presence: true
+    validates :poll_id, presence: true
 
+    has_many :answer_choices,
+        class_name: :AnswerChoice,
+        primary_key: :id,
+        foreign_key: :question_id
+    
+    belongs_to :owner_poll,
+        class_name: :Poll,
+        primary_key: :id,
+        foreign_key: :poll_id
+        
 end
