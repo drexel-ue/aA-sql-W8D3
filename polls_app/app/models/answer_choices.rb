ccede440fa92e33d1d85f50ade9a4bbd4280f4da
ikesh pack <packappsforyou@gmail.com>
2019-06-12 19:05:00 -0400

    Created models and annotated

---
+++ polls_app/app/models/answer_choices.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: answer_choices
+#
+#  id          :bigint           not null, primary key
+#  question_id :integer          not null
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class AnswerChoices < ApplicationRecord
+
+
+
+end
---
+++ polls_app/app/models/question.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :bigint           not null, primary key
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  poll_id    :integer          not null
+#
+
+class Question < ApplicationRecord
+
+end
